df327111e0f6a3d4f77559b1f735bd4c26fc6d46
MiguelJerome <sharkmaycry@gmail.com>
2022-07-02 18:58:11 -0400

    point class

---
+++ lab05/lab05/Point.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel;
+
+class Point
+    {
+
+        public int VerifierIntervalle(int plageValeur)
+        {
+            const int INTERVALEL_MAX = 10;
+            const int INTERVALLE_MIN = -10;
+            const int EN_DEHORS_INTERVALLE = 0;
+
+            if (plageValeur < INTERVALLE_MIN && plageValeur > INTERVALEL_MAX)
+            {
+                return EN_DEHORS_INTERVALLE;
+            }
+
+            return plageValeur;
+        }
+        public Point()
+        {
+            this._abcisse = 1;
+            this._ordonne = 1;
+        }
+
+        public Point(int abcisse)
+        {
+            this._abcisse = this.VerifierIntervalle(abcisse);
+            this._ordonne = 1;
+        }
+
+        public Point(int abcisse, int ordonne)
+        {
+            this._abcisse = this.VerifierIntervalle(abcisse);
+            this._ordonne = this.VerifierIntervalle(ordonne);
+        }
+
+        public String ToString()
+        {
+            String stringPoint = "[Abcisse = " + this._abcisse + ", Ordonne = " + this._ordonne + "]";
+            return stringPoint;
+
+        }
+
+        public void Translater(int vx, int vy)
+        {
+            this._abcisse += vx;
+            this._ordonne += vy;
+        }
+
+        public double Distance(Point p)
+        {
+            return Math.Sqrt( Math.Pow((this._abcisse - p._abcisse),2) + Math.Pow((this._ordonne-p._ordonne),2));
+        }
+
+        public double Distance(int abcisse, int ordonne)
+        {
+            Point p = new Point(abcisse, ordonne);
+            p._ordonne = p.VerifierIntervalle(ordonne);
+            p._abcisse =p.VerifierIntervalle(abcisse);
+            return Math.Sqrt(Math.Pow((this._abcisse - p._abcisse), 2) + Math.Pow((this._ordonne - p._ordonne), 2));
+        }
+     
+        public void Egualite(Point p)
+        {
+            if (this._ordonne == p._ordonne && this._abcisse == p._abcisse)
+            {
+                Console.WriteLine($"Les deux points sont egaux");
+            }
+            else
+            {
+                Console.WriteLine($"Les deux employes ne sont pas");
+            }
+
+        }
+
+
+
+
+        private int _abcisse;
+        private int _ordonne;
+    }
+
+        
--- lab05/lab05/Program.cs
+++ lab05/lab05/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+var p1 = new Point();
+var p2 = new Point(5);
+var p3 = new Point( 2, 5);
+var p4 = new Point( 4,6);
+
+p1.Translater(2,3);
+Console.WriteLine($"Apres le Translater de p1 est : {p1.ToString()}");
+Console.WriteLine($"La distance entre p1 et p3 est : {p1.Distance(p3)}");
+Console.WriteLine($"La distance entre p1 le point abcisse = 6 et oronnee = 4 est : {p1.Distance(6,4)}");
+
+// J ai fait les 7 premiere questions de Test
+
+// egualite testing
+var p5 =new Point(5,5);
+var p6 =new Point(5,5);
+var p7 = new Point(1, 120);
+Console.WriteLine();
+Console.WriteLine($"Les points p5 et p6 : ");
+p5.Egualite(p6);
+
+Console.WriteLine();
+Console.WriteLine($"Les points p5 et p6 : ");
+p5.Egualite(p7);
+
