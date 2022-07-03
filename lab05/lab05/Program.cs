7e23a929699816615398d56e2e3b55eb7fd1a73b
MiguelJerome <sharkmaycry@gmail.com>
2022-07-02 21:28:01 -0400

    upadated class Point

--- lab05/lab05/Program.cs
+++ lab05/lab05/Program.cs
@@ -14,14 +14,14 @@ Console.WriteLine($"La distance entre p1 le point abcisse = 6 et oronnee = 4 est
 // J ai fait les 7 premiere questions de Test
 
 // egualite testing
-var p5 =new Point(5,5);
-var p6 =new Point(5,5);
+var p5 = new Point(5,5);
+var p6 = new Point(5,5);
 var p7 = new Point(1, 120);
 Console.WriteLine();
 Console.WriteLine($"Les points p5 et p6 : ");
 p5.Egualite(p6);
 
 Console.WriteLine();
-Console.WriteLine($"Les points p5 et p6 : ");
+Console.WriteLine($"Les points p5 et p7 : ");
 p5.Egualite(p7);
 
